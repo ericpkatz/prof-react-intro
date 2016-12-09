d752f42af40b82adf755ee02c387ade95b8050b1
Eric P. Katz <ericpkatz@gmail.com>
2016-12-09 11:35:51 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+var root = document.getElementById('root');
+
+const Header = ()=> <h1>My App </h1> ;
+
+const TodosList = ({ todos })=> {
+  const lis = todos.map( (todo, idx) => <li key={ idx }>{ todo }</li> );
+  return (
+      <ul>
+        { lis }
+      </ul>
+  );
+}
+
+class App extends Component{
+  constructor(){
+    super();
+    const todos = [ 'Foo', 'Bar' ];
+    this.state = { todos };
+
+  }
+  render(){
+    return (
+        <div>
+          <Header />
+          <TodosList todos={ this.state.todos } />
+        </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, root);
+console.log('fizz buzz');
